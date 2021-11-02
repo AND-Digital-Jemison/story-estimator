f1871d196411aa382e1c7da5a87475e357455ae3
Dale Waterworth <dale.waterworth@and.digital>
2021-11-02 14:52:41 +0000

    set web socket adapter

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,12 +1,18 @@
 import { NestFactory } from '@nestjs/core';
-
+import { WsAdapter } from '@nestjs/platform-ws';
+import { ConfigService } from '~/modules/config/config.service';
 import { APIModule } from './modules/api.module';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(APIModule);
-  const config = app.get('ConfigService').envConfig;
-  app.enableCors({ origin: config.CORS_WHITELIST.split(',') });
-  await app.listen(config.PORT);
+
+  const config = app.get<ConfigService>(ConfigService);
+
+  app.enableCors({ origin: config.corsWhiteList });
+
+  app.useWebSocketAdapter(new WsAdapter(app));
+
+  await app.listen(config.port);
 }
 
 bootstrap();
