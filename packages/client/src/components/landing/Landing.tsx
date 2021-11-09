5cd70ed3da7dc7b3909ef0f22fd4ac80bb997987
shuyanliu <clemcodes@outlook.com>
2021-11-09 11:50:33 +0000

    center box

--- packages/client/src/components/landing/Landing.tsx
+++ packages/client/src/components/landing/Landing.tsx
@@ -5,18 +5,37 @@ import styled from 'styled-components';
 
 const Title = styled.h1`
   color: #2897ff;
+  margin: 0;
+`;
+
+const Body = styled.body`
+  background-color: #2897ff;
+  margin: auto;
+  width: auto;
+  height: 2000px;
+  padding: 100px;
+`;
+
+const Main = styled.div`
+  background-color: #fff;
+  margin: auto;
+  width: 598px;
+  height: 672px;
+  border-radius: 43px;
 `;
 
 export const Landing = () => {
   return (
-    <>
-      <Title>Planning Poker</Title>
-      <CreateGame />
-      <form>
-        <label>Got a room code?</label>
-        <input type="text" name="room-code-name" />
-        <button>JOIN</button>
-      </form>
-    </>
+    <Body>
+      <Main>
+        <Title>Planning Poker</Title>
+        <CreateGame />
+        <form>
+          <label>Got a room code?</label>
+          <input type="text" name="room-code-name" />
+          <button>JOIN</button>
+        </form>
+      </Main>
+    </Body>
   );
 };
