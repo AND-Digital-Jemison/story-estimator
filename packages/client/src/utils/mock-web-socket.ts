41bd797a85add740c82fc9f798432527835f2a9a
Jack Rimmer <jack.rimmer@and.digital>
2021-11-16 16:56:06 +0000

    Used correct server socket url

--- packages/client/src/utils/mock-web-socket.ts
+++ packages/client/src/utils/mock-web-socket.ts
@@ -1,4 +1,4 @@
 import WS from 'jest-websocket-mock';
 
-export const mockSocketUrl = 'ws://localhost:8000';
+export const mockSocketUrl = 'ws://localhost:8001';
 export const mockSocketServer = new WS(mockSocketUrl);
