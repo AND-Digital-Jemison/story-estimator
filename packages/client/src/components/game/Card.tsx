b476e0e983898b417cd903aec302791b8051f220
nimra ajmal <nimra.ajmal@and.digital>
2021-11-24 15:14:44 +0000

    looping logic for cards

--- packages/client/src/components/game/Card.tsx
+++ packages/client/src/components/game/Card.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { CardBox } from './game-styles';
 
-export const Card = () => {
+export const Card = props => {
   return <CardBox>?</CardBox>;
 };
--- packages/client/src/components/game/Game.tsx
+++ packages/client/src/components/game/Game.tsx
@@ -4,13 +4,22 @@ import { Story } from './Story';
 import { Card } from './Card';
 
 export const Game = () => {
+  // const [users, setUsers] = useState([]);
+  const users = [
+    { id: 1, name: 'shuyan', selected: false },
+    { id: 2, name: 'nimra', selected: false },
+    { id: 3, name: 'jack', selected: false },
+    { id: 4, name: 'dale', selected: false },
+  ];
   return (
     <GameContainer>
       <RoomCode>Room Code: 4a5b</RoomCode>
       <div>
         <Title>Planning Poker</Title>
         <Story />
-        <Card />
+        {users.map(user => (
+          <Card key={user.id} user={user} />
+        ))}
       </div>
     </GameContainer>
   );
