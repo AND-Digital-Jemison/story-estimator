7f866d9bb75e9787bbc76cf0a5c63147f77c1883
Imran Azam <imranazam3@gmail.com>
2022-03-14 14:47:18 +0000

    formatting fix (align with prettier rules)

--- packages/client/src/components/landing/create-game/CreateGame.tsx
+++ packages/client/src/components/landing/create-game/CreateGame.tsx
@@ -1,13 +1,22 @@
-import React, { useState } from "react";
-import { useHistory } from "react-router";
-import { Message } from "../../hooks/web-socket/types";
-import { LandingEvent } from "../event-constants";
-import { ButtonContainer, CreateButton, FormContainer, Input, Label } from "../landing-styles";
-import { CreateData } from "../types";
+import React, { useState } from 'react';
+import { useHistory } from 'react-router';
+import { Message } from '../../hooks/web-socket/types';
+import { LandingEvent } from '../event-constants';
+import {
+  ButtonContainer,
+  CreateButton,
+  FormContainer,
+  Input,
+  Label,
+} from '../landing-styles';
+import { CreateData } from '../types';
 
-export const CreateGame = (props: { name: string, validateNameCallback: (nameValid: boolean) => void }) => {
+export const CreateGame = (props: {
+  name: string;
+  validateNameCallback: (nameValid: boolean) => void;
+}) => {
   const { name, validateNameCallback } = props;
-  const [story, setStory] = useState("");
+  const [story, setStory] = useState('');
   const history = useHistory();
 
   const handleClick = (e: any) => {
@@ -15,16 +24,16 @@ export const CreateGame = (props: { name: string, validateNameCallback: (nameVal
     const data: CreateData = {
       name: name,
       story: story,
-      event: LandingEvent.CREATE
+      event: LandingEvent.CREATE,
     };
 
     if (inputValid(data)) {
       const message: Message = {
-        event: "story-event-listener",
-        data: data
+        event: 'story-event-listener',
+        data: data,
       };
 
-      console.log("create", message);
+      console.log('create', message);
 
       history.push('/game', message);
     }
@@ -49,7 +58,9 @@ export const CreateGame = (props: { name: string, validateNameCallback: (nameVal
         onChange={(e: any) => setStory(e.target.value)}
       />
       <ButtonContainer>
-        <CreateButton onClick={(e: any) => handleClick(e)}>CREATE GAME</CreateButton>
+        <CreateButton onClick={(e: any) => handleClick(e)}>
+          CREATE GAME
+        </CreateButton>
       </ButtonContainer>
     </FormContainer>
   );
