c9f7dca3b609ce200b1a50d6e85aa067e3e02841
Imran Azam <imranazam3@gmail.com>
2022-03-14 15:26:17 +0000

    generate unique game id and with consistent length (4 alphanumeric chars)

--- packages/server/src/modules/story-estimator/story-game.repository.ts
+++ packages/server/src/modules/story-estimator/story-game.repository.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { Game } from "./classes/dto/Game";
+import { Game } from './classes/dto/Game';
 
 @Injectable()
 export class StoryGameRepository {
   private games: Game[] = [];
 
-  public getGame(id: string) {
+  public getGame(id: string): Game {
     console.log('find game', id);
     for (const game of this.games) {
       if (game.session.id === id) {
@@ -15,8 +15,12 @@ export class StoryGameRepository {
     throw new Error('Could not find game');
   }
 
-  public addGame(game: Game) {
+  public addGame(game: Game): void {
     this.games.push(game);
     console.log('All games', this.games);
+  }
+
+  public getExistingGameIds(): string[] {
+    return this.games.map(game => game.session.id);
   }
 }
