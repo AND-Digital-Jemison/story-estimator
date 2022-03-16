6904038b7ddfe8d90fc4cd1dcb6f695cc478a464
Imran Azam <imranazam3@gmail.com>
2022-03-16 15:42:58 +0000

    typo fix

--- packages/server/src/modules/story-estimator/games-manager.service.ts
+++ packages/server/src/modules/story-estimator/games-manager.service.ts
@@ -8,20 +8,20 @@ export class GamesManagerService {
   private readonly logger = new Logger('Manager');
   private GAME_TTL = 0.002; // games with last interaction > 5 hours will be deleted
 
-  constructor(private storyGameIdGeneratorService: StoryGameRepository) {}
+  constructor(private storyGameRepository: StoryGameRepository) {}
 
   @Cron(CronExpression.EVERY_SECOND)
   handleCron(): void {
     const now = new Date().getTime();
     const gamesActiveTimes =
-      this.storyGameIdGeneratorService.getGamesLastInteractionMap();
+      this.storyGameRepository.getGamesLastInteractionMap();
 
     gamesActiveTimes.forEach(game => {
       const gameHours = Math.abs(now - game.lastInteraction) / 36e5;
 
       if (gameHours > this.GAME_TTL) {
         this.logger.log(`deleting game:  ${game.gameId}`);
-        this.storyGameIdGeneratorService.deleteGame(game.gameId);
+        this.storyGameRepository.deleteGame(game.gameId);
       }
     });
   }
